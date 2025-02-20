d7e64407f67f412d6f19abeb268fcecff2615e55
Vaibav <vvaibav03@gmail.com>
2025-02-20 14:56:55 +0530

    added analytics and speed insight tags

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -25,6 +27,8 @@ export default function RootLayout({ children }) {
             >
                 {children}
                 <Toaster />
+                <Analytics />
+                <SpeedInsights />
             </body>
         </html>
     );

1c1455067c1c92a14bfbe8bac97b50fd85fb3b1b
sspenterprisescontact-a11y <sspenterprises.contact@gmail.com>
2025-09-06 17:41:43 +0530

    Changes-To Change the footer content

--- app/Components/rights.tsx
+++ app/Components/rights.tsx
@@ -12,7 +12,15 @@ export default function Rights() {
         <div
             className={`md:flex text-sm text-center md:justify-evenly text-gray-500 bg-black pt-8 pb-8 pl-14 pr-14 ${cardo.className}`}
         >
-            <p>© 2025 GKP & Sons Jewellers | All Rights Reserved.</p>
+            <p>
+                © Developed by{" "}
+                <a
+                    href="https://wa.me/919363167299?text=HI%20Young%20Mynds%20Infotech.%20I%20am%20interested%20in%20your%20service"
+                    className="cursor-pointer hover:underline ml-1"
+                >
+                    Young Mynds Infotech.
+                </a>{" "}
+            </p>
             <p>
                 <a href="/privacy" className="cursor-pointer hover:underline">
                     Privacy Policy
